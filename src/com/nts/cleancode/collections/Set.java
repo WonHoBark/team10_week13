ddd318063c956a6f10fb45f8f0157feb34794b37
hyeonwoo <chw1787@naver.com>
2016-06-06 15:59:51 +0900

    Extract method in set class.

--- src/com/nts/cleancode/collections/Set.java
+++ src/com/nts/cleancode/collections/Set.java
@@ -11,20 +11,33 @@ public class Set extends AbstractSet {
 	}
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			if (newSize > elements.length) {
-				Object[] newElements =
-					new Object[elements.length + INITIAL_CAPACITY];
-				for (int i = 0; i < size; i++)
-					newElements[i] = elements[i];
-				elements = newElements;
-			}
+		if (contains(element))
+			return;
+		
+		if (readOnly)
+			return;
 
-			if (contains(element))
-				return;
-			elements[size++] = element;
+		if (shouldGrow()) {
+			grow();
 		}
+
+		addElement(element);
+
+	}
+
+	protected void addElement(Object element) {
+		elements[size++] = element;
+	}
+
+	protected void grow() {
+		Object[] newElements = new Object[elements.length + INITIAL_CAPACITY];
+		for (int i = 0; i < size; i++)
+			newElements[i] = elements[i];
+		elements = newElements;
+	}
+
+	protected boolean shouldGrow() {
+		return size + 1 > elements.length;
 	}
 
 	public boolean contains(Object element) {
